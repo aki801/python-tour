1faf05f662d3843c26efdd62f83a25265127c61f
kary <kairy159@gmail.com>
2015-06-01 10:38:12 -0500

    se agregan las ultimas lineas, problemas con espcios con letra

--- Sesion06/orca.py
+++ Sesion06/orca.py
@@ -23,15 +23,12 @@ P = random.choice(B)
 # Se calula la longitud de la palabra seleccionada
 np=len(P)
 
-# La siguiente línea no es necesaria ya que np ya es entero
-# E=int(np) # convierte np a entero ¿cuanto vale np? A esta altura no se sabe
-
 # La siguiente línea que hace? o para qué?
 lp = list(P)
 
 # Se crea una cadena con espacios de la misma longitud de la
 # palabra seleccionada
-Esp = "_"*np
+Esp = " _ "*np
 
 # dead tiene cada uno de los estados de la muerte del monito
 dead = [" O \n   \n   \n  \n"," O \n/  \n   \n   \n"," O \n/| \n   \n   \n"," O \n/|\\\n   \n   \n"," O \n/|\\\n |\n   \n"," O \n/|\\\n |\n/  \n"," O \n/|\\\n |\n/ \\\n"," O \n/|\\\n |\n/ \\\n"]
@@ -39,9 +36,6 @@ dead = [" O \n   \n   \n  \n"," O \n/  \n   \n   \n"," O \n/| \n   \n   \n"," O 
 # I es la longitud de dead y es el número máximo de intentos que tiene
 # el usuario para adivinar la palabra
 I = len(dead)
-
-# La siguiente línea me parece que no tiene uso y sentido, así que la comento
-# dead=str()
 
 # horca contiene el ascii para puntar la horca y es un string, lo cambio!
 horca = " +----+ \n |    | \n |      \n |      \n |      \n========"
@@ -63,12 +57,16 @@ L = raw_input() #lee algo de teclado
 if L in P: # revisa si existe la letra en la palabra
   # si existe, entonces hay que buscar en que posición o posiciones
   # está la letra en toda la palabra, así que hay que recorrerla toda
-  i=0
-  while i<np:
-    if L == P[i]: # si la letra L está ne la posición i
-      # entonces hay que remplazarla en la variable de espacios
-      Esp[i] = L
-    i+=1
+	i=0
+	while i<np:
+		if L == P[i]: # si la letra L está np la posición i
+      		# entonces hay que remplazarla en la variable de espacios
+		Esp[i] = L 
+		i+=1
+	print Esp
 else:
-  intentos+=1
-  
+	if intentos>7:
+		print dead[intentos]
+		intentos+=1
+		for intentos=7:
+			print 'Perdiste'
